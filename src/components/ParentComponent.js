085ade23dba4114ac2ed07385dfe885634a95d30
steffent1493 <steffent1493@gmail.com>
2021-07-16 14:26:38 -0400

    mission accomplished

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,10 +1,9 @@
-  import React, { useState } from "react";
-  import Form from "./Form";
-  import DisplayData from "./DisplayData";
-  
-function ParentComponent() {
+import React, { useState } from "react";
+
+function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -14,18 +13,34 @@ function ParentComponent() {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
     <div>
-      <Form
-        firstName={firstName}
-        lastName={lastName}
-        handleFirstNameChange={handleFirstNameChange}
-        handleLastNameChange={handleLastNameChange}
-      />
-      <DisplayData firstName={firstName} lastName={lastName} />
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Submissions</h3>
+      {listOfSubmissions}
     </div>
   );
-} 
+}
 
-export default ParentComponent;
-
+export default Form; 
